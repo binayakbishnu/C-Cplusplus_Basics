8556557a3945c9781f2b2ca79c41fbf016b4535b
Binayak Bishnu <71837066+BinayakBishnu@users.noreply.github.com>
2021-03-17 15:44:16 +0530

    Add files via upload

---
+++ Non-Primitive->Linear->Arrays/dynamic_in_C.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    printf("Enter number: ");
+    scanf(" %d",&n);
+
+    int *ptr;
+    ptr = (int *)malloc(n*sizeof(int));
+    //ptr=(int*)calloc(n,sizeof(int));
+
+    if (ptr==NULL){
+        printf("Memory not allocated");
+        exit(0);
+    }
+    else{
+        int i;
+        for (i=0;i<n;i++)
+        {
+            ptr[i] = i+1;
+        }
+        for (i =0;i<n;i++)
+        {
+            printf("%d ",ptr[i]);
+        }
+    }
+
+    return 0;
+}
